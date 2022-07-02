a44cae1c93f729da5c452a8caef012886b7e708f
Misha <noderoid64@gmail.com>
2022-07-02 18:27:42 +0200

    (Server) (Feature) Add simpleinjector

---
+++ PersonalSite/Infrastructure/Simpleinject/SimpleInjectExtension.cs
@@ -0,0 +1,24 @@
+﻿using SimpleInjector;
+using SimpleInjector.Lifestyles;
+
+namespace PersonalSite.Infrastructure.SimpleInject;
+
+public static class SimpleInjectExtension
+{
+    public static void AddSimpleInjectorContainer(this IServiceCollection sc, Container container)  
+    {        
+        container.Options.DefaultLifestyle = Lifestyle.Scoped;  
+        container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();  
+
+        // Here should be registrations
+        // container.Register<ILogger, ConsoleLogger>();  
+  
+        sc.AddSimpleInjector(container, op =>  
+        {  
+            op.AddAspNetCore().AddControllerActivation();  
+        });
+        
+        
+  
+    }
+}
--- PersonalSite/Program.cs
+++ PersonalSite/Program.cs
@@ -1,13 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using PersonalSite.Infrastructure.SimpleInject;
+using SimpleInjector;
 
+var builder = WebApplication.CreateBuilder(args);
+var container = new Container();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSimpleInjectorContainer(container);
 
 var app = builder.Build();
+app.Services.UseSimpleInjector(container);
+container.Verify();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
